50a647bf3478bb59ce64a49e2c97661290015e34
Taylor Parsell <tayparsell@gmail.com>
2017-01-12 07:16:51 -0800

    updating TEST_DB_URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://user1:user1password@ds161038.mlab.com:61038/blog-goose';
-exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL || 'mongodb://user1:user1password@ds161038.mlab.com:61038/blog-goose'); 
+exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL || 'mongodb://user1:user1password@ds019708.mlab.com:19708/grilledcheese'); 
 exports.PORT = process.env.PORT || 8080;
